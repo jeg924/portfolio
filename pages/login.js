13b8aaed8b1e7ce632f67e22f0913c2888c13005
James <jeg924@gmail.com>
2021-08-24 16:39:10 -0400

    did some work to the login screen.

--- pages/login.js
+++ pages/login.js
@@ -15,22 +15,30 @@ import {
   Text,
   View,
   ScrollView,
+  TextInput,
 } from "react-native-web";
+import { MdAccountCircle } from "react-icons/md";
+import { GiRobotGolem } from "react-icons/gi";
 import MyButton from "components/MyButton";
 
 export default function Login() {
+  const [signIn, setSignIn] = React.useState(true);
+  const [signUp, setSignUp] = React.useState(false);
+
+  async function loginWithEmailAndPassword() {}
+
+  async function createAnAccountWithEmailAndPassword() {}
+
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: "green",
         flexDirection: "column",
       }}
     >
       <View
         style={{
           flexDirection: "row",
-          backgroundColor: "yellow",
           height: 50,
           marginTop: 0,
         }}
@@ -64,7 +72,118 @@ export default function Login() {
           </Link>
         </View>
       </View>
-      <View></View>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+            }}
+          >
+            <GiRobotGolem style={{ width: 30, height: 30 }} />
+            <Text
+              style={{
+                fontSize: 30,
+                fontFamily: "helvetica",
+                fontWeight: "bold",
+                fontStyle: "italic",
+              }}
+            >
+              {" "}
+              Game CMS
+            </Text>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+        </View>
+        <View style={{ flex: 3, flexDirection: "row" }}>
+          <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+          <View
+            style={{
+              flex: 1.5,
+              backgroundColor: "blue",
+              borderWidth: 1,
+              borderColor: "black",
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                backgroundColor: "cyan",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ fontSize: 30 }}>Sign In</Text>
+              <Text style={{ fontSize: 30 }}>Sign Up</Text>
+            </View>
+            <View style={{ flex: 4, backgroundColor: "white" }}>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <MdAccountCircle style={{ width: 50, height: 50 }} />
+              </View>
+              <View
+                style={{
+                  flex: 3,
+                  backgroundColor: "tan",
+                }}
+              >
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                  <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+                  <View style={{ flex: 1 }}></View>
+                  <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+                </View>
+                <View style={{ flex: 4, flexDirection: "row" }}>
+                  <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+                  <View style={{ justifyContent: "space-around" }}>
+                    <TextInput
+                      placeholder="email"
+                      style={{
+                        backgroundColor: "white",
+                        borderWidth: 1,
+                        padding: 6,
+                      }}
+                    />
+                    <TextInput
+                      placeholder="password"
+                      style={{
+                        backgroundColor: "white",
+                        borderWidth: 1,
+                        padding: 6,
+                      }}
+                    />
+                    <MyButton title="Go" onPress={() => {}} />
+                  </View>
+                  <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+                </View>
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                  <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+                  <View style={{ flex: 1 }}></View>
+                  <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={{ flex: 1, backgroundColor: "red" }}></View>
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+          <View style={{ flex: 1 }}></View>
+          <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+        </View>
+      </View>
     </View>
   );
 }
